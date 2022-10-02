a3795998026a71c70aa7116c3cf8eabf7ba641bc
abusa <abusa@RaselPC>
2022-10-02 14:12:40 -0400

    commit 5

--- TechShop_Project/src/main/java/com/smarttechqa/utils/Utilities.java
+++ TechShop_Project/src/main/java/com/smarttechqa/utils/Utilities.java
@@ -1,8 +1,14 @@
 package com.smarttechqa.utils;
 
+import java.awt.Desktop.Action;
+
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.gargoylesoftware.htmlunit.javascript.background.JavaScriptExecutor;
 
 import SmartTechQA.BaseClass;
 
@@ -13,6 +19,27 @@ public class Utilities extends BaseClass{
 		WebElement elem = wait.until(ExpectedConditions.elementToBeClickable(element));
 		return elem;
 	}
+	
+	public static void actionClick(WebElement element) {
+		Actions action = new Actions(driver);
+		action.click(element).build().perform();
+	}
+	
+	public static void jsClick(WebElement element) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeAsyncScript("arguments[0].click();", element);
+	}
+	
+	public static void elementScroll(WebElement element) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeAsyncScript("arguments[0].scrollIntoView", element);
+	}
+	public static void scrollToBottomPage(WebElement element) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeAsyncScript(null, element);
+	}
+	
+	
 	}
 
 
